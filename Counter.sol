9934dd8489e8de02f45ba08fcf8d50bb50b85c62
cdrmack <git.pynvt@passmail.net>
2024-05-22 12:00:24 +0200

    add constructor to the Counter

--- Counter.sol
+++ Counter.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.26;
 contract Counter {
 	int32 number = 0;
 
+	constructor(int32 _initialNumber) {
+		number = _initialNumber;
+	}
+
 	function get() public view returns (int32) {
 		return number;
 	}
