cec1e1dd3b588e404f8bcce4198f3312a8caea75
cdrmack <git.pynvt@passmail.net>
2024-06-18 15:47:08 +0200

    add chainlink aggregator interface to FundMe

--- FundMe.sol
+++ FundMe.sol
@@ -1,10 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
     uint256 minimumEth = 1;
 
     function fund() public payable {
         require(msg.value >= minimumEth, "didn't send enough ETH"); // 1e18 = 1 ETH
+    }
+
+    function get_version() public view returns (uint256) {
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version(); // ETH/USD Price Feed, Sepolia Test Network
     }
 }
