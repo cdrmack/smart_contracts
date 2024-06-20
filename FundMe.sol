5a0c83c4022be02be6c2337be7e0e6a052c4025e
cdrmack <git.pynvt@passmail.net>
2024-06-20 17:47:19 +0200

    check ETH/USD on chainlink

--- FundMe.sol
+++ FundMe.sol
@@ -4,13 +4,13 @@ pragma solidity ^0.8.0;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
-    uint256 minimumEth = 1;
+    uint256 public constant MINIMUM_USD = 5e18; // 5 * 10 ** 18
 
     address[] public funders;
     mapping(address => uint256)  public fundersMap;
 
     function fund() public payable {
-        require(msg.value >= minimumEth, "didn't send enough ETH"); // 1e18 = 1 ETH
+        require(get_conversion_rate(msg.value) >= MINIMUM_USD, "Didn't send enough ETH!");
         funders.push(msg.sender);
         fundersMap[msg.sender] += msg.value;
     }
@@ -19,7 +19,24 @@ contract FundMe {
         return funders.length;
     }
 
-    function get_version() public view returns (uint256) {
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version(); // ETH/USD Price Feed, Sepolia Test Network
+    /*
+    * USD/ETH - 18 decimals
+    * ETH/USD - 8 decimals
+    * can be checked with .decimals() on contract itself
+    */
+    function get_price() public view returns (uint256) {
+        // Sepolia ETH/USD Address
+        // https://docs.chain.link/data-feeds/price-feeds/addresses
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        // https://docs.chain.link/data-feeds/api-reference#getrounddata
+        (, int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function get_conversion_rate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = get_price();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
     }
 }
